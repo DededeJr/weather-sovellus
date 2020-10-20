12d4884af116330b9f32bdbb28214d4e3d812050
CT-5555 <miro.vallden@gmail.com>
2020-10-20 18:17:04 +0300

    paljon divejä

--- weather/src/Weather.js
+++ weather/src/Weather.js
@@ -5,12 +5,39 @@ class Weather extends Component {
     return (
       <div className="Weather">
         <div className="Weather_focused">
+
           <div className="Weather_day">ma</div>
           <div className="Weather_time">12.00</div>
           <div className="Weather_temp">14</div>
           <img src='http://openweathermap.org/img/wn/09d.png' alt="Logo" />
         </div>
+
+        <div className="Weather_forecast">
+
+         <div className= "Weather_box">
+          <div className="Weather_day">ke</div>
+          <div className="Weather_time">11.00</div>
+          <div className="Weather_temp">12</div>
+          <img src='http://openweathermap.org/img/wn/09d.png' alt="Logo" />
+        </div>
+
+        <div className= "Weather_box">
+         <div className="Weather_day">pe</div>
+         <div className="Weather_time">2.00</div>
+         <div className="Weather_temp">-3</div>
+         <img src='http://openweathermap.org/img/wn/09d.png' alt="Logo" />
+       </div>
+
+       <div className= "Weather_box">
+        <div className="Weather_day">ti</div>
+        <div className="Weather_time">15.00</div>
+        <div className="Weather_temp">17</div>
+        <img src='http://openweathermap.org/img/wn/09d.png' alt="Logo" />
       </div>
+
+      </div>
+
+    </div>
     )
   }
 }
