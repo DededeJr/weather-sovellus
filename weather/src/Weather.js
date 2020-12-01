bf6cf7b7fdfe4d3755990ee66746e12844e60611
miro.vallden <e@e.e>
2020-12-01 18:34:39 +0200

    paljon säätöä

--- weather/src/Weather.js
+++ weather/src/Weather.js
@@ -9,12 +9,13 @@ class Weather extends Component {
     super(props)
     this.state = {
       forecasts: null
+      focusedForecastDt: null,
     }
   }
 
   componentDidMount(){
     getForecast().then(forecasts => {
-      this.setState({ forecasts: forecasts})
+      this.setState({ forecasts: forecasts, focusedForecastDt: forecast.dt})
     })
   }
 
@@ -38,7 +39,7 @@ class Weather extends Component {
   renderWeatherItem(forecast) {
     return (
 
-      <div className= "Weather_box">
+      <div className= "Weather_box" key={forecast.dt} onClick={() => this.changeFocusedForecast(forecast)}>
         <div className="Weather_day">{this.formatDay(forecast.dt)}</div>
         <div className="Weather_time">{this.formatTime(forecast.dt)}</div>
         <div className="Weather_temp">{Math.round(forecast.main.temp)}</div>
@@ -49,10 +50,11 @@ class Weather extends Component {
 
   render() {
     const forecasts =this.state.forecasts
+    const focusedForecastDt = this.state.focusedForecastDt
     if(!forecasts) return null
     console.log(forecasts)
 
-    const focusedWeather = forecasts.list[0]
+    const focusedWeather = forecasts.list.find(f => f.dt === focusedForecastDt)
 
     return (
       <div className="Weather">
@@ -61,6 +63,7 @@ class Weather extends Component {
           <div className="Weather_day">{this.formatDay(focusedWeather.dt)}</div>
           <div className="Weather_time">{this.formatTime(focusedWeather.dt)}</div>
           <div className="Weather_temp">{Math.round(focusedWeather.main.temp)}</div>
+          <div className="Weather_desc">{focusedWeather.weather[0].description}</div>
           <img src={'http://openweathermap.org/img/wn/'+focusedWeather.weather[0].icon+'.png'} alt="Logo" />
         </div>
 
