c78532a23c684b9b37141633eb47e4ba4eb438de
miro.vallden <e@e.e>
2020-12-08 18:02:50 +0200

    Updates

---
+++ precache-manifest.0c2c22e5c430edb7a672bf918aea9692.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92e79468085047722f7cc2e0a4afa5d1",
+    "url": "/weather-sovellus/index.html"
+  },
+  {
+    "revision": "b8e3abac212b47d4fde6",
+    "url": "/weather-sovellus/static/css/main.ed9af1d0.chunk.css"
+  },
+  {
+    "revision": "514ae3c554f8f7879f25",
+    "url": "/weather-sovellus/static/js/2.2bfdb317.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-sovellus/static/js/2.2bfdb317.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8e3abac212b47d4fde6",
+    "url": "/weather-sovellus/static/js/main.9bce03a7.chunk.js"
+  },
+  {
+    "revision": "6e47d707f4a59d12313e",
+    "url": "/weather-sovellus/static/js/runtime-main.022940f0.js"
+  }
+]);
